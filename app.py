10b1183556978b580c60f4b5c95b7bab8f516eb9
Duriya Gosonhemmanee <duriya.0577@gmail.com>
2024-02-11 12:20:28 +0700

    create login and register route

--- app.py
+++ app.py
@@ -18,6 +18,14 @@ class  MyForm(FlaskForm) :
 def index() :
     return render_template('index.html',)
 
+@app.route('/login')
+def login() :
+    return render_template('login.html',)
+
+@app.route('/register')
+def register() :
+    return render_template('register.html',)
+
 @app.route('/second', methods = ['GET','POST'])
 def second() :
     form = MyForm()
